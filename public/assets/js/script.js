1e379dec9bf8982fcbe250529cb89b8e76b235a5
Brooke Wildermuth <wildermuthbrooke@gmail.com>
2018-03-18 20:59:04 -0500

    Project Files Completed

---
+++ public/assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function (){
+    $("body").on("click", "#text-enter-button", function (event) {
+        var burgerData = {
+            name: $("#burger-text").val().trim()
+        };
+        $.post("/", burgerData).done(function (data) {
+            getCall();
+        });
+    });
+
+    function getCall() {
+        $.get("/").done(function (data) {
+            var HTML = $.parseHTML(data, document, true);
+            var bodyHTML = HTML[15].innerHTML
+            console.log(HTML);
+            $(".container-fluid").html(bodyHTML);
+        });
+    }
+});
